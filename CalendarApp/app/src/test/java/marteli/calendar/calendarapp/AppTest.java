de8510edd59f6106e426637d80eade7db7a3a4a4
Victor Marteli <19598552@student.curtin.edu.au>
2023-10-15 14:44:27 +0800

    minor

--- CalendarApp/app/src/test/java/marteli/calendar/calendarapp/AppTest.java
+++ CalendarApp/app/src/test/java/marteli/calendar/calendarapp/AppTest.java
@@ -3,8 +3,8 @@
  */
 package marteli.calendar.calendarapp;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     // @Test void appHasAGreeting() {
